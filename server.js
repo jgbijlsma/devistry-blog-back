afb8bc95735cad2040696245b1c5e627b143e6cd
jgbijlsma <jbijlsma.p@gmail.com>
2020-03-26 11:19:17 +0100

    initial project

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// set up express
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+console.log("Server starting...");
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+// set up mongoose
+
+console.log("Connecting to MongoDB...");
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  err => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
