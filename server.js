f9b8671ca0b05c9c559a03389c06cbde94ff0a8b
jgbijlsma <jbijlsma.p@gmail.com>
2020-03-29 09:42:07 +0200

    add article model and router

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 console.log("Server starting...");
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
+// set up routes
+
+app.use("/articles", require("./routes/articleRouter"));
+
 // set up mongoose
 
 console.log("Connecting to MongoDB...");
@@ -20,7 +24,8 @@ mongoose.connect(
   process.env.MONGODB_CONNECTION_STRING,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   },
   err => {
     if (err) throw err;
